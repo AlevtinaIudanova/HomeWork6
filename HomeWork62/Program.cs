815b29d3d9f5649dbd5dfdb6ce210963a99efcf5
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-28 21:51:29 +0100

    2

---
+++ HomeWork62/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = i + j;
+            //Console.Write($"{matr[i, j]} ");
+        }
+    }
+}
+int[,] matrix = new int[3, 4];
+
+
+//PrintArray(matrix);
+Console.WriteLine();
+FillArray(matrix);
+
+PrintArray(matrix);
+Console.WriteLine();
