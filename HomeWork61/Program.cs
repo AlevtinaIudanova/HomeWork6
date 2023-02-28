e2ca255c2e0624084a043bac5cb55eaf0ff9cab7
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-28 21:50:02 +0100

    1

---
+++ HomeWork61/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Показать двумерный массив размером m×n заполненный вещественными числами*/
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().NextDouble();
+            //Console.Write($"{matr[i, j]} ");
+        }
+    }
+}
+
+double[,] matrix = new double[3, 4];
+
+//PrintArray(matrix);
+Console.WriteLine();
+FillArray(matrix);
+
+PrintArray(matrix);
